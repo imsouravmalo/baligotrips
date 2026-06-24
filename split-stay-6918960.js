806bd67dcfdf7a26b5d2d71055b5ec9fbccab7d3
Sourav Malo <sourav.cse5.bu@gmail.com>
2026-06-24 09:24:10 +0600

    6951595 and 6918960 modified

--- split-stay-6918960.js
+++ split-stay-6918960.js
@@ -1,28 +1,32 @@
+var timeout;
+
 function roundAndSeparate() {
-	
-  //---Add only thousands separators----
-  
-  /* Offer Price IDR (Breakfast) 99332759 -> 99482258 (first number is formula, second is short text) 
-  Offer Price IDR (Half Board)   99136529 -> 99482259
-  Offer Price IDR (Full Board)   99136530 -> 99482261 */
-  
-  //   var number1 = loader.engine.document.getElementById(99332759).getProperty('value.value');
-  // loader.engine.document.getElementById(99482258).setValue(({"value": number1.toLocaleString('en-US')})); //- adds thousand separators
-  
-  //   var number2 = loader.engine.document.getElementById(99136529).getProperty('value.value');
-  // loader.engine.document.getElementById(99482259).setValue(({"value": number2.toLocaleString('en-US')})); //- adds thousand separators
-  
-  //   var number3 = loader.engine.document.getElementById(99136530).getProperty('value.value');
-  // loader.engine.document.getElementById(99482261).setValue(({"value": number3.toLocaleString('en-US')})); 
+	clearTimeout(timeout);
+
+  timeout = setTimeout(function() {
+    //---Add only thousands separators----
     
-  //---Round up AND add thousands separators----
-  
-  /* INR Cost (Breakfast Plan)                99117086 -> 99482262
-  INR Cost ( Breakfast Plan - India Billing ) 99391056 -> 99482263
-  INR Cost(Half Board Plan)                   99390975 -> 99482265
-  INR Cost ( Half Board  - India Billing )    99391060 -> 99391060
-  INR Cost ( Full Board Plan)                 99390976 -> 99482272
-  INR Cost ( Full Board - India Billing )     99391070 -> 99482273 */
+    /* Offer Price IDR (Breakfast) 99332759 -> 99482258 (first number is formula, second is short text) 
+    Offer Price IDR (Half Board)   99136529 -> 99482259
+    Offer Price IDR (Full Board)   99136530 -> 99482261 */
+    
+    //   var number1 = loader.engine.document.getElementById(99332759).getProperty('value.value');
+    // loader.engine.document.getElementById(99482258).setValue(({"value": number1.toLocaleString('en-US')})); //- adds thousand separators
+    
+    //   var number2 = loader.engine.document.getElementById(99136529).getProperty('value.value');
+    // loader.engine.document.getElementById(99482259).setValue(({"value": number2.toLocaleString('en-US')})); //- adds thousand separators
+    
+    //   var number3 = loader.engine.document.getElementById(99136530).getProperty('value.value');
+    // loader.engine.document.getElementById(99482261).setValue(({"value": number3.toLocaleString('en-US')})); 
+      
+    //---Round up AND add thousands separators----
+    
+    /* INR Cost (Breakfast Plan)                99117086 -> 99482262
+    INR Cost ( Breakfast Plan - India Billing ) 99391056 -> 99482263
+    INR Cost(Half Board Plan)                   99390975 -> 99482265
+    INR Cost ( Half Board  - India Billing )    99391060 -> 99391060
+    INR Cost ( Full Board Plan)                 99390976 -> 99482272
+    INR Cost ( Full Board - India Billing )     99391070 -> 99482273 */
   
     var result1 = loader.engine.document.getElementById(121427311).getProperty('value.value');
     var roundedValue1 = Math.ceil(result1); //- rounding to next integer
@@ -119,7 +123,8 @@ function roundAndSeparate() {
 	var result1 = loader.engine.document.getElementById(121431271).getProperty('value.value');
     var roundedValue1 = Math.ceil(result1); //- rounding to next integer
   loader.engine.document.getElementById(121431272).setValue(({"value": roundedValue1.toLocaleString('en-US')})); //- adds thousand separators
-  };
+  }, 100)
+};
   
   document.addEventListener('focusout', function(e) {
     if (e.target.matches('input, textarea, select')) {
@@ -134,4 +139,6 @@ function roundAndSeparate() {
       roundAndSeparate();
     }
   });
+
+  document.addEventListener('click', roundAndSeparate);
 
--- split-stay-6951595.js
+++ split-stay-6951595.js
@@ -1,28 +1,33 @@
+var timeout;
+
 function roundAndSeparate() {
-	
-  //---Add only thousands separators----
-  
-  /* Offer Price IDR (Breakfast) 99332759 -> 99482258 (first number is formula, second is short text) 
-  Offer Price IDR (Half Board)   99136529 -> 99482259
-  Offer Price IDR (Full Board)   99136530 -> 99482261 */
-  
-  //   var number1 = loader.engine.document.getElementById(99332759).getProperty('value.value');
-  // loader.engine.document.getElementById(99482258).setValue(({"value": number1.toLocaleString('en-US')})); //- adds thousand separators
-  
-  //   var number2 = loader.engine.document.getElementById(99136529).getProperty('value.value');
-  // loader.engine.document.getElementById(99482259).setValue(({"value": number2.toLocaleString('en-US')})); //- adds thousand separators
-  
-  //   var number3 = loader.engine.document.getElementById(99136530).getProperty('value.value');
-  // loader.engine.document.getElementById(99482261).setValue(({"value": number3.toLocaleString('en-US')})); 
+	clearTimeout(timeout);
+
+  timeout = setTimeout(function() {
+    //---Add only thousands separators----
+    //---Add only thousands separators----
     
-  //---Round up AND add thousands separators----
-  
-  /* INR Cost (Breakfast Plan)                99117086 -> 99482262
-  INR Cost ( Breakfast Plan - India Billing ) 99391056 -> 99482263
-  INR Cost(Half Board Plan)                   99390975 -> 99482265
-  INR Cost ( Half Board  - India Billing )    99391060 -> 99391060
-  INR Cost ( Full Board Plan)                 99390976 -> 99482272
-  INR Cost ( Full Board - India Billing )     99391070 -> 99482273 */
+    /* Offer Price IDR (Breakfast) 99332759 -> 99482258 (first number is formula, second is short text) 
+    Offer Price IDR (Half Board)   99136529 -> 99482259
+    Offer Price IDR (Full Board)   99136530 -> 99482261 */
+    
+    //   var number1 = loader.engine.document.getElementById(99332759).getProperty('value.value');
+    // loader.engine.document.getElementById(99482258).setValue(({"value": number1.toLocaleString('en-US')})); //- adds thousand separators
+    
+    //   var number2 = loader.engine.document.getElementById(99136529).getProperty('value.value');
+    // loader.engine.document.getElementById(99482259).setValue(({"value": number2.toLocaleString('en-US')})); //- adds thousand separators
+    
+    //   var number3 = loader.engine.document.getElementById(99136530).getProperty('value.value');
+    // loader.engine.document.getElementById(99482261).setValue(({"value": number3.toLocaleString('en-US')})); 
+      
+    //---Round up AND add thousands separators----
+    
+    /* INR Cost (Breakfast Plan)                99117086 -> 99482262
+    INR Cost ( Breakfast Plan - India Billing ) 99391056 -> 99482263
+    INR Cost(Half Board Plan)                   99390975 -> 99482265
+    INR Cost ( Half Board  - India Billing )    99391060 -> 99391060
+    INR Cost ( Full Board Plan)                 99390976 -> 99482272
+    INR Cost ( Full Board - India Billing )     99391070 -> 99482273 */
   
     var result1 = loader.engine.document.getElementById(121283229).getProperty('value.value');
     var roundedValue1 = Math.ceil(result1); //- rounding to next integer
@@ -59,7 +64,8 @@ function roundAndSeparate() {
 	var result1 = loader.engine.document.getElementById(121322019).getProperty('value.value');
     var roundedValue1 = Math.ceil(result1); //- rounding to next integer
   loader.engine.document.getElementById(121322022).setValue(({"value": roundedValue1.toLocaleString('en-US')})); //- adds thousand separators
-  };
+  }, 100);
+}
 
   function handleSpecialCaseForRadioButtons(el) {
     var targetedParent = el.closest('[data-type="radio"][data-id]');
@@ -92,4 +98,6 @@ function roundAndSeparate() {
       }
     }
   });
+
+  document.addEventListener('click', roundAndSeparate);
 
